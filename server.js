f7a5cb0ecbd932823bdd0996688ad968df174130
Adityakachare <78196873+Adityakachare@users.noreply.github.com>
2023-10-27 02:37:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //Configure env
 dotenv.config();
 
 //Database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Rest Object
 const app = express();
